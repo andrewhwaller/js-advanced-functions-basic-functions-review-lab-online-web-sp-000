14e8e9463bab05f787ad33d49d4f962b4fa7f712
Andrew Waller <andrewhwaller@gmail.com>
2019-10-01 22:40:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ let wrapAdjective = function(style="*") {
 
 const Calculator = {
   add: function(a, b) {
-    return a + b
+    return a + b;
   }
   subtract: function(a, b) {
-    return a - b
+    return a - b;
   }
   multiply: function(a, b) {
-    return a * b
+    return a * b;
   }
   divide: function(a, b) {
-    return a/b
+    return a / b;
   }
 }
